80a872742724ddb4d1579f8c53c0c031b10230b3
NanaMiyakoshi <not9yrv@gmail.com>
2020-04-29 02:51:55 +0900

    チェックボックスのテスト追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,6 @@ class SessionsController < ApplicationController
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      remember user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'

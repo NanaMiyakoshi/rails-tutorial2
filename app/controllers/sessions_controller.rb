bb96f35239671b0b863357b86f252e5e099faa2e
NanaMiyakoshi <not9yrv@gmail.com>
2020-05-06 20:12:26 +0900

    有効でないユーザーはログイン不可に変更

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,9 +8,16 @@ class SessionsController < ApplicationController
    #if ユーザーがデータベースにあり、かつ、認証に成功するか
     if user && user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message  = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = 'Invalid email/password combination'
       #flash.now[:danger] = user.errors.full_messages

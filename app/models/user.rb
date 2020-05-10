79a55c0ea609167aceea67297a9d955b5ef0eb77
NanaMiyakoshi <not9yrv@gmail.com>
2020-05-11 01:00:29 +0900

    削除されたユーザーのマイクロポストも削除されるようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :microposts
+    has_many :microposts, dependent: :destroy
     include Gravtastic
     gravtastic
     attr_accessor :remember_token, :activation_token, :reset_token
